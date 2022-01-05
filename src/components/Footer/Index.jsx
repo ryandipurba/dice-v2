51898bf57189bc902dc585d8d8079132b81dcd0a
ryandi <purbaryandi@gmail.com>
2022-01-05 13:37:47 +0700

    fiinish v2

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -1,81 +1,31 @@
 import React from "react";
 import "./style.css";
+import Discord from "../../assets/image/discord.png";
+import Twitter from "../../assets/image/twitter.png";
 
 export default function Footer() {
   return (
-    <footer
-      className="text-center text-white sticky-bottom"
-      // style={{ backgroundColor: "#f1f1f1" }}
-    >
-      {/* <!-- Grid container --> */}
-      <div className="container pt-4">
-        {/* <!-- Section: Social media --> */}
-        <section className="mb-4">
-          {/* <!-- Facebook --> */}
+    <div className="container">
+      <div className="col-md-12 text-center mb-3">
+        <span className="mr-5">
           <a
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://www.facebook.com/groups/1220649054627343"
-            role="button"
-            data-mdb-ripple-color="dark"
+            href="https://twitter.com/dicedegens"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className="fab fa-facebook-f"></i>
+            <img src={Twitter} alt="Twitter" className="twitter-icon" />
           </a>
-
-          {/* <!-- Twitter --> */}
+        </span>
+        <span>
           <a
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://twitter.com/quantumQTC"
-            role="button"
-            data-mdb-ripple-color="dark"
+            href="https://discord.gg/Ym9VETwz"
+            target="_blank"
+            rel="noreferrer"
           >
-            <i className="fab fa-twitter"></i>
+            <img src={Discord} alt="Discord" className="discord-icon" />
           </a>
-
-          {/* <!-- Youtobe --> */}
-          <a
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://www.youtube.com/channel/UC4tTzBhauqPY9BFfsWBsFDQ"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fab fa-youtube"></i>
-          </a>
-
-          {/* <!-- Instagram --> */}
-          <a
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://www.instagram.com/quantumqtc/"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fab fa-instagram"></i>
-          </a>
-
-          {/* <!-- website --> */}
-          <a
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="https://www.quantum-coin.co/"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fab fa-safari"></i>
-          </a>
-        </section>
-        {/* <!-- Section: Social media --> */}
+        </span>
       </div>
-      {/* <!-- Grid container --> */}
-
-      {/* <!-- Copyright --> */}
-      <div
-        className="text-center text-dark p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        © 2021 Copyright:
-        <a className="text-dark" href="https://www.quantum-coin.co/">
-          &nbsp; Quantum world
-        </a>
-      </div>
-      {/* <!-- Copyright --> */}
-    </footer>
+    </div>
   );
 }
